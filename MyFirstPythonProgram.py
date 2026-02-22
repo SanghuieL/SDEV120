80f430759a5ddd5fedee10c4ea389e07bfc597f4
Seth Lee <sanghuie.lee@gmail.com>
2026-02-21 19:21:46 -0500

    Add files via upload
    Module 6 Assignment IDLE Python

---
+++ MyFirstPythonProgram.py
@@ -0,0 +1,20 @@
+print("Hello World")
+print("I just wrote my first Python Program")
+print("This is so much fun")
+
+""" This code will
+
+Prompt for your name
+
+set the "name' variable and print a message."""
+
+name = input("Hello, what is your name? ")  #gets the user's name
+
+print("Welcome, it is so nice to meet you " + name) #prints a message with the user's name
+
+#The following assignment will print to the screen: "It is raining cats and dogs." including the appropriate spacing.
+
+c = "cats"
+d = "dogs"
+s = "It is raining " + c + " and " + d + "."
+print(s)
