afc4b1f7ad61afe1cb4a735ae14fd2b0eecde2e8
Seth Lee <sanghuie.lee@gmail.com>
2026-03-04 01:32:40 -0500

    finish sdevProject.py?

--- sdevProject.py
+++ sdevProject.py
@@ -1,13 +1,20 @@
-def dataEntry(): 
+import pandas
+
+def dataEntry(checks): 
+    
     person = employee()
-    inputValid = validationModule(person)
+    inputValid = validationModule(person, checks)
+    checks.append(inputValid)
+    
     if(inputValid):
         #save employee data
-        print("Employee data input successfully.")
-        return person
+        person.dependents = int(person.dependents)
+        person.employeeID = int(person.employeeID)
+        #print("Employee data input successfully.\n")
+        return person, 1
     else:
-        print("Invalid input. Please re-enter employee information.")
-        return 0
+        print("Invalid input. Please re-enter employee information.\n")
+        return person, 0
 
 class employee:
     def __init__(self):
@@ -20,11 +27,11 @@ class employee:
         self.firstName = first
         self.lastName = last
         self.employeeID = ID
-        self.dependents = int(dep)
+        self.dependents = dep
         self.hoursWorked = float(hours)
         self.hourlyRate = 0 #check if add later
         
-def validationModule(employee):
+def validationModule(employee, checks):
     valid = True
     
     if not employee.firstName:
@@ -36,28 +43,31 @@ def validationModule(employee):
     if not employee.employeeID.isnumeric():
         print("Employee ID must be a number.")
         valid = False
+    if not employee.dependents.isnumeric():
+        print("Dependents must be a number")
+        valid = False
     if employee.hoursWorked < 0 or employee.hoursWorked > 80:
         print("Hours worked must be between 0 and 80.")
         valid = False
         
-    employee.employeeID = int(employee.employeeID)
-    
     return valid
 
-def payRateLookup(eID, database):
+def payRateLookup(eID, database, checks):
     
     payRateFound = False
     
     for record in database:
-        if record.employeeID == eID:
-            hourlyRate = record.hourlyRate
+        if record == eID:
+            hourlyRate = database[record]
             payRateFound = True
             break
+    
+    checks.append(payRateFound)
     
     if payRateFound:
         return hourlyRate
     else:
-        print("Error: Invalid Employee ID.")
+        print("Error: Invalid Employee ID.\n")
         return 0
     
 def payCalculation(person):
@@ -74,18 +84,72 @@ def payCalculation(person):
     total_deductions = state_tax + federal_tax + dependent_deduction
     net_pay = gross_pay - total_deductions
     
+    #print("finish payCalculation")
     return gross_pay, total_deductions, net_pay
 
-def output():
+def addToArray(person, record, check, row, gross = 0, deductions = 0, net = 0):
     
+    if gross:
+        printOutput(person, gross, deductions, net)
     
-    
+    record.append(data(person, gross, deductions, net, check))
+    #print("finish addToArray")
     return
 
+def printOutput(person, gross, deductions, net):
+    
+    print(person.firstName, person.lastName, person.employeeID, person.dependents)
+    print(person.hourlyRate, person.hoursWorked)
+    print(gross, deductions, net)
+
+    return
+
+def data(person, gross, deductions, net, checks):
+    
+    out = [person.firstName, person.lastName, person.employeeID, person.dependents, person.hoursWorked]
+    out.append(checks[0])
+    if len(checks)> 1:
+        out.append(person.hourlyRate)
+        out.append(checks[1])
+    if gross:
+        out.append(gross)
+        out.append(deductions)
+        out.append(net)
+        
+    return out
+
 if __name__ == "__main__":
-    person = dataEntry()
-    #person.hourlyRate = payRateLookup(person.employeeID, database)
-    gross, deductions, net = payCalculation(person)
-    output(person)
+    payRate = {0:23, 1:7.25, 2:12.50, 3:15, 4:9.60, 5:19.40, 
+               6:12.65, "0007":11.85, 8:16.60, 9:18, 10:15.35}
+    file = r'C:\Users\slee\Desktop\result.xlsx'
+    counter = 0
+    record = []
+    
+    start = input("Is there an employee to create a report for? (y/n) ")
+    while start.lower() == "y":
+        check = []
+        person, good = dataEntry(check)
+        if (not good):
+            addToArray(person, record, check, counter)
+            counter += 1
+            continue
+        
+        person.hourlyRate = payRateLookup(person.employeeID, payRate, check)
+        if (not person.hourlyRate):
+            addToArray(person, record, check, counter)
+            counter += 1
+            continue
+        
+        gross, deductions, net = payCalculation(person)
+        addToArray(person, record, check, counter, gross, deductions, net)
+        
+        counter += 1
+        start = input("Is there another employee to create a report for? (y/n) ")
+    
+    df = pandas.DataFrame(record, columns=["First Name", "Last Name", "Employee ID", "Dependents",
+                                           "Hours Worked", "Input Valid Check", "Hourly Rate", "Valid ID Check"
+                                           "Gross Pay", "Deductions", "Net Pay"]).T
+    df.to_excel(excel_writer=file)
+    print("End of program \n results saved at" + file)
     
 
