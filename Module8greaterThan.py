677528b5ab8ff3124ed894192212c9e77ed93c8f
Seth Lee <sanghuie.lee@gmail.com>
2026-03-07 14:30:02 -0500

    Module 8 assignment

---
+++ Module8greaterThan.py
@@ -0,0 +1,10 @@
+def greater_than(x,y):
+    if x > y:
+        return "true"
+    else:
+        return "false"
+
+a = 10
+b = 6
+result = greater_than(a,b)
+print("The statement " + str(a) + " is greater than " + str(b) + " is " + str(result) )
